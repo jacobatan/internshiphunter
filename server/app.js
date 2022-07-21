e6de17bdfd1ad8af55d212357a717c71da8b01e0
Jacob Tan-WSL <jacob.tan229@gmail.com>
2022-07-21 17:33:24 +1200

    feat: add python method calls to node

--- server/app.js
+++ server/app.js
@@ -1,1 +1,10 @@
-console.log("Hello world!");
+const { spawn } = require("child_process");
+const py = spawn("python3", ["./python/index.py", "apple sauce!"]);
+
+py.stdout.on("data", (data) => {
+  console.log(data.toString());
+});
+
+py.on("close", (code) => {
+  console.log(`child process exited with code${code}`);
+});
