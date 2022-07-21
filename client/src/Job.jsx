70d8673d61198de087e1273773d559edbde893ee
nirajpatilcs <62448681+nirajpatilcs@users.noreply.github.com>
2022-07-21 19:49:19 +1200

    initial

---
+++ client/src/Job.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const Job = () => {
+    return(
+        <div>
+            <h1 className="jobs-found">
+                Here are the jobs we found for you...
+            </h1>
+        </div>
+    )
+}
+
+export default Job; 
