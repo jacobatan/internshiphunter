636aafa81f867cb011677a8073a2578a0e8ee316
nirajpatilcs <62448681+nirajpatilcs@users.noreply.github.com>
2022-07-21 20:09:14 +1200

    router fail

--- client/src/pages/Jobs/index.jsx
+++ client/src/pages/Jobs/index.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import "./index.css";
+import Button from "../../components/Button";
 
 const Jobs = () => {
-  return <div>Jobs</div>;
+
+  function handleChange() {
+    console.log('hi');
+  }
+
+  return (
+    <div>
+        <Button text={"hello!"} handleChange={handleChange()}/>
+    </div>
+  )
 };
 
 export default Jobs;
