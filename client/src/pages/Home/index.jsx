a3d2785a8ba6123efcb178021078ab48205e4f38
dhruvkanth <100739087+dhruvkanth@users.noreply.github.com>
2022-07-22 18:52:39 +1200

    home page v2

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import "./index.css";
-import Input from "./Input";
+import {Navigate, Route, Routes} from "react-router-dom";
+
+import Index from "/Users/dhruvkanthpuli/Documents/wdcc/internshiphunter/client/src/components/Signup";
+import Index from "/Users/dhruvkanthpuli/Documents/wdcc/internshiphunter/client/src/components/Login";
 
 const Home = () => {
-  return (
-      <div className="home">
-        <Input/>
-      </div>
-  )
+    return (
+        <Routes>
+            <Route path="/signup" exact element={<Index />} />
+            <Route path="/login" exact element={<Index />} />
+            <Route path="/" element={<Navigate replace to="/login" />} />
+        </Routes>
+    );
 };
 
 export default Home;
