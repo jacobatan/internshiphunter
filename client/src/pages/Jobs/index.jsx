acd5052ad0b2b814fcfb0f54653f9d693bc5b065
nirajpatilcs <62448681+nirajpatilcs@users.noreply.github.com>
2022-07-22 22:31:30 +1200

    job page implemented

--- client/src/pages/Jobs/index.jsx
+++ client/src/pages/Jobs/index.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import "./index.css";
 import Button from "../../components/Button";
+import Spinner from "../../components/Spinner"
+import JobCard from '../../components/JobCard'
+import { useEffect } from "react";
+import { useState } from "react";
 
 const Jobs = () => {
 
@@ -8,9 +12,52 @@ const Jobs = () => {
     console.log('hi');
   }
 
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const loader = setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+
+    return () => clearTimeout(loader);
+  }, []);
+  
+
   return (
-    <div>
-        <Button text={"hello!"} handleChange={handleChange()}/>
+    <div className="jobs">
+        {/* <Button text={"hello!"} handleChange={handleChange()}/> */}  
+      <h1 className="jobs-found">
+          Here are the jobs we found for you...
+      </h1>
+      {
+        loading ? 
+          <div className="loading">
+            <Spinner> </Spinner>
+          </div>
+        : 
+          <div className="show-jobs">
+                <JobCard 
+                  title="Google"
+                  content="hi"
+                  subtitle="software intern"
+                />
+                <JobCard 
+                  title="Google"
+                  content="be pro software engineer"
+                  subtitle="software intern"
+                />
+                <JobCard 
+                  title="Google"
+                  content="be pro software engineer"
+                  subtitle="software intern"
+                />
+            </div>
+      }
+
+  
+      
+      
+      
     </div>
   )
 };
