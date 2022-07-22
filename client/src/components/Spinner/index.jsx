3e7629805dda8b05c85f7ed803812866be31a8e1
nirajpatilcs <62448681+nirajpatilcs@users.noreply.github.com>
2022-07-22 22:31:30 +1200

    job page implemented

---
+++ client/src/components/Spinner/index.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./index.css";
+
+const Spinner = (props) => {
+  return(
+    <div class="spinner">
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  );
+};
+
+export default Spinner;
