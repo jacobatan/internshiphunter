5ef4869bd394cff5893c87d6f64532d9f60f386c
Jacob Tan-WSL <jacob.tan229@gmail.com>
2022-07-23 21:29:04 +1200

    styles: login

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./global.css";
-import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Signup from "./components/Signup";
 import Login from "./components/Login";
+import Jobs from "./pages/Jobs";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <Routes>
-        <Route path="/signup" exact element={<Signup />} />
-        <Route path="/login" exact element={<Login />} />
-        <Route path="/" element={<Navigate replace to="/login" />} />
+      <Route path="/signup" exact element={<Signup />} />
+      <Route path="/login" exact element={<Login />} />
+      <Route path="/" element={<Navigate replace to="/login" />} />
+      <Route path="/jobs" element={<Jobs />} />
     </Routes>
   </BrowserRouter>
 );
