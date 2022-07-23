8b6dcda2dbcd4317b96f9e9ecf72e9f2eae5498c
dhruvkanth <100739087+dhruvkanth@users.noreply.github.com>
2022-07-23 21:19:42 +1200

    Dhruv home (#7)
    * home page
    
    * home page v2
    
    * idk what this is lol
    
    * latest push
    
    * style: sing -> sign
    
    Co-authored-by: Jacob Tan-WSL <jacob.tan229@gmail.com>

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import "./index.css";
+import {Navigate, Route, Routes} from "react-router-dom";
+
+import Index from "/Users/dhruvkanthpuli/Documents/wdcc/internshiphunter/client/src/components/Signup";
+import Index from "/Users/dhruvkanthpuli/Documents/wdcc/internshiphunter/client/src/components/Login";
 
 const Home = () => {
-  return <div>Home</div>;
+    return (
+        <Routes>
+            <Route path="/signup" exact element={<Index />} />
+            <Route path="/login" exact element={<Index />} />
+            <Route path="/" element={<Navigate replace to="/login" />} />
+        </Routes>
+    );
 };
 
 export default Home;
