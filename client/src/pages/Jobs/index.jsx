e7bbca07cf104373d617686481662b55103a613f
dhruvkanth <100739087+dhruvkanth@users.noreply.github.com>
2022-07-23 20:31:53 +1200

    dk push latest

--- client/src/pages/Jobs/index.jsx
+++ client/src/pages/Jobs/index.jsx
@@ -19,21 +19,21 @@ const Jobs = () => {
     const names = ["Jacob", "Aditya", "Niraj", "Ayaan", "DK"];
 
     const showEmoteBtns = names.map((name, i) => {
-      return (
-        <span key={i}><Button text="emote" id={i} onClick={i=>emotesOnClick(i)}></Button></span>
-      )
+        return (
+            <span key={i}><Button text="emote" id={i} onClick={i => emotesOnClick(i)}></Button></span>
+        )
     })
 
     const showNames = names.map((name, i) => {
-      return (
-          <span key={i}><p className="name" >{name}</p></span>
-      )
+        return (
+            <span key={i}><p className="name">{name}</p></span>
+        )
     })
 
-    const showScores = scores.map((score, i)=> {
-      return (
-        <p key={i} className="score"> {score} </p>
-      )
+    const showScores = scores.map((score, i) => {
+        return (
+            <p key={i} className="score"> {score} </p>
+        )
     })
 
     function onClick() {
@@ -41,7 +41,8 @@ const Jobs = () => {
     }
 
     const emotesOnClick = (event) => {
-      console.log(event.currentTarget.id);
+        setShowEmotes(!showEmotes);
+        showModal(!modal);
     }
 
 
@@ -85,7 +86,7 @@ const Jobs = () => {
                             <h2 className="leaderboard">Leaderboard</h2>
                             <div className="modal-leaderboard-box">
                                 <div className="modal-leaderboard-section">
-                                    <span><h4 className="heading" >Name</h4></span>
+                                    <span><h4 className="heading">Name</h4></span>
                                     {showNames}
                                 </div>
                                 <div className="modal-leaderboard-section">
@@ -103,9 +104,41 @@ const Jobs = () => {
             }
 
             {
-            showEmotes &&
-                <div className="emotes-modal">
-
+                showEmotes &&
+                <div id="myModal" className="modal">
+                    <div className="modal-content">
+                        <span onClick={emotesOnClick} className="close">&times;</span>
+                        <div className="modal-leaderboard">
+                            <h1>Points : 400</h1>
+                            <div className="modal-leaderboard-box">
+                                <div className="modal-leaderboard-section">
+                                    <span><img src="https://c.tenor.com/bmFPh10gQtQAAAAC/salman-khan.gif"
+                                               className="profile-img" alt='salman khan'/></span>
+                                    <span><Button text="SEND"></Button></span>
+                                    <span><img src="https://c.tenor.com/WxxgkUaLvCsAAAAC/ghanta-salman.gif"
+                                               className="profile-img" alt='salman khan'/></span>
+                                    <span><Button text="Buy : 50PP"></Button></span>
+                                </div>
+                                <div className="modal-leaderboard-section">
+                                    <span><img src="https://c.tenor.com/w2mCAR7kgUsAAAAC/clash-royale-emotes.gif"
+                                               className="profile-img" alt='salman khan'/></span>
+                                    <span><Button text="SEND"></Button></span>
+                                    <span><img
+                                        src="https://c.tenor.com/YvwNrCfhvOIAAAAC/susu-varudhu-raghava-lawrence.gif"
+                                        className="profile-img" alt='salman khan'/></span>
+                                    <span><Button text="Buy : 50PP"></Button></span>
+                                </div>
+                                <div className="modal-leaderboard-section">
+                                    <span><img src="https://c.tenor.com/6GjVJS0Fn5cAAAAd/hila-lund.gif"
+                                               className="profile-img" alt='salman khan'/></span>
+                                    <span><Button text="SEND"></Button></span>
+                                    <span><img src="https://c.tenor.com/XvJfOBkN33MAAAAM/modi-ji-tapa-tapa.gif"
+                                               className="profile-img" alt='salman khan'/></span>
+                                    <span><Button text="Buy : 50PP"></Button></span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             }
 
